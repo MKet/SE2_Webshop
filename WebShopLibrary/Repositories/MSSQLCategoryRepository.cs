fe3d5f07585ace95523a5ec75bfaeffcab5fc340
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-05-23 15:41:33 +0200

    add mssql implementation of the category and product repositories

---
+++ WebShopLibrary/Repositories/MSSQLCategoryRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using WebShopLibrary.Entities;
+
+namespace WebShopLibrary.Repositories
+{
+    class MSSQLCategoryRepository : ICategoryRepository
+    {
+        private string ConnectionString;
+
+        public MSSQLCategoryRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public IReadOnlyCollection<Category> GetCategories(int Skip, int Amount)
+        {
+            var categories = new List<Category>();
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = @"SELECT *
+                                    FROM categories 
+                                    ORDER BY ID desc
+                                    OFFSET (@skip) ROWS FETCH NEXT (@take) ROWS ONLY";
+                command.Parameters.AddWithValue("@skip", Skip);
+                command.Parameters.AddWithValue("@take", Amount);
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                        categories.Add(Convert(reader));
+            }
+            return categories.AsReadOnly();
+        }
+
+        public IReadOnlyCollection<Category> GetSubCategoriesOf(int Category, int Skip, int Amount)
+        {
+            var categories = new List<Category>();
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = @"SELECT *
+                                    FROM products 
+                                    ORDER BY ID desc
+                                    WHERE SubCategoryOf = @Category
+                                    OFFSET (@skip) ROWS FETCH NEXT (@take) ROWS ONLY";
+                command.Parameters.AddWithValue("@skip", Skip);
+                command.Parameters.AddWithValue("@take", Amount);
+                command.Parameters.AddWithValue("@Category", Category);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                        categories.Add(Convert(reader));
+            }
+            return categories.AsReadOnly();
+        }
+
+        public void Insert(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Category Convert(SqlDataReader reader)
+        {
+            return new Category()
+            {
+                Id = (int)reader["ID"],
+                SubCategoryOf = (int)reader["SubCategoryOf"],
+                Name = reader["Name"].ToString(),
+                Text = reader["Text"].ToString(),
+            };
+        }
+    }
+}
---
+++ WebShopLibrary/Repositories/MSSQLProductRepository.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using WebShopLibrary.Entities;
+using WebShopLibrary.Exceptions;
+
+namespace WebShopLibrary.Repositories
+{
+    class MSSQLProductRepository : IProductRepository
+    {
+        private string ConnectionString;
+
+        public MSSQLProductRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public Product GetProduct(int Id)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = @"Select *  
+                                        FROM products 
+                                        where ID = @ID";
+
+                command.Parameters.AddWithValue("@ID", Id);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                        return Convert(reader);
+            }
+            throw new ItemNotFoundException();
+        }
+
+        public IReadOnlyCollection<Product> GetProducts(int Skip, int Amount)
+        {
+            var products = new List<Product>();
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = @"SELECT *
+                                    FROM products 
+                                    ORDER BY ID desc
+                                    OFFSET (@skip) ROWS FETCH NEXT (@take) ROWS ONLY";
+                command.Parameters.AddWithValue("@skip", Skip);
+                command.Parameters.AddWithValue("@take", Amount);
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                        products.Add(Convert(reader));
+            }
+            return products.AsReadOnly();
+        }
+
+        public IReadOnlyCollection<Product> GetProducts(int Category, int Skip, int Amount)
+        {
+            var products = new List<Product>();
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = @"SELECT *
+                                    FROM products 
+                                    WHERE Category = @category 
+                                    ORDER BY ID desc
+                                    OFFSET (@skip) ROWS FETCH NEXT (@take) ROWS ONLY";
+                connection.Open();
+
+                command.Parameters.AddWithValue("@skip", Skip);
+                command.Parameters.AddWithValue("@take", Amount);
+                command.Parameters.AddWithValue("@category", Category);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                        products.Add(Convert(reader));
+            }
+            return products.AsReadOnly();
+        }
+
+        public void insert(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Product Convert(SqlDataReader reader)
+        {
+            return new Product()
+            {
+                Id = (int)reader["ID"],
+                Name = reader["Name"].ToString(),
+                Image = reader["Image"].ToString(),
+                Location = reader["Location"].ToString(),
+                Author = reader["Author"].ToString(),
+                Text = reader["Text"].ToString(),
+                Price = (double)reader["Price"],
+                Category = (int)reader["Category"],
+                Isvisible = (bool)reader["Visible"],
+                Discount = reader["code"].ToString()
+            };
+        }
+    }
+}
