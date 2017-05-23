1bdca062cd2bcded6777fd062e93007d0e47cd12
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-05-23 15:48:30 +0200

    Add GetPage method to CatalogService

--- WebShopLibrary/Services/CatalogService.cs
+++ WebShopLibrary/Services/CatalogService.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebShopLibrary.Entities;
+using WebShopLibrary.Repositories;
 
 namespace WebShopLibrary.Services
 {
     public class CatalogService
     {
+        private int pageSize;
+        private IProductRepository productRepository;
+        private ICategoryRepository categoryRepository;
+
+        public CatalogService(IProductRepository productRepository, ICategoryRepository categoryRepository, int pageSize = 30)
+        {
+            this.pageSize = pageSize;
+            this.productRepository = productRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IReadOnlyCollection<Product> GetPage(int page) =>
+            productRepository.GetProducts(page * pageSize, pageSize);
     }
 }
