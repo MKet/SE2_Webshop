733f271cd171d397880da5a0f2ada26dbd74388b
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-05-23 15:39:15 +0200

    rename GetProductsByCategory in product repository to getProducts

--- WebShopLibrary/Repositories/IProductRepository.cs
+++ WebShopLibrary/Repositories/IProductRepository.cs
@@ -10,6 +10,6 @@ namespace WebShopLibrary.Repositories
         void insert(Product product);
         IReadOnlyCollection<Product> GetProducts(int Skip, int Amount);
         Product GetProduct(int ID);
-        IReadOnlyCollection<Product> GetProductsByCategory(int Category, int Skip, int Amount);
+        IReadOnlyCollection<Product> GetProducts(int Category, int Skip, int Amount);
     }
 }
