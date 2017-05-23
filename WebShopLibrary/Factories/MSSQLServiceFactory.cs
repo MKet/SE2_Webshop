9ecf0043ee75f83fb14b268c16faa5c1090e17ef
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-05-23 15:48:06 +0200

    add MssqlServiceFactory and implement CreateCatalogService

---
+++ WebShopLibrary/Factories/MSSQLServiceFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebShopLibrary.Repositories;
+using WebShopLibrary.Services;
+
+namespace WebShopLibrary.Factories
+{
+    class MSSQLServiceFactory : IServiceFactory
+    {
+        private string ConnectionString = @"Integrated Security=true; Database=EbookStore; Data Source=MARCO-LAPTOP"; 
+        public CatalogService CreateCatalogService()
+        {
+            return new CatalogService(new MSSQLProductRepository(ConnectionString), new MSSQLCategoryRepository(ConnectionString));
+        }
+
+        public OrderService CreateOrderService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserService CreateUserService()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
