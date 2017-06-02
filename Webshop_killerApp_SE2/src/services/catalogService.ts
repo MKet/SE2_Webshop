e9d37f9dda5c4b3dd82db3d53d48910ec7e2f34f
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-06-02 12:24:05 +0200

    change clientside api call to use post body for parameters

--- Webshop_killerApp_SE2/src/services/catalogService.ts
+++ Webshop_killerApp_SE2/src/services/catalogService.ts
@@ -9,7 +9,11 @@ export class catalogService {
     }
 
     public async getPage(number: number): Promise<Product[]> {
-        let response: Response = await this.http.fetch('catalog/' + number.toString());
+        let response: Response = await this.http.fetch('catalog/products', {
+            body: json({
+                "Item1": 1
+            })
+        });
         return await response.json();
     }
 
