ee74eeb1ee1bae44bd19b64105dccef684b1832d
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-06-08 01:23:51 +0200

    implement review list in productDetail component

--- Webshop_killerApp_SE2/src/components/productDetail.ts
+++ Webshop_killerApp_SE2/src/components/productDetail.ts
@@ -1,16 +1,19 @@
 import { autoinject } from 'aurelia-framework';
 import { catalogService } from '../services/catalogService';
 import { Product } from '../entities/Product';
+import { Review } from '../entities/Review';
 
 @autoinject
 export class ProductDetail {    
 
     private product: Product;
+    private reviews: Review[];
 
     constructor(private catalogService: catalogService) { }
 
     async activate(params, routeConfig) {
         console.info(params.id);
         this.product = await this.catalogService.getProduct(params.id);
+        this.reviews = await this.catalogService.getReviews(params.id);
     }
 }
--- Webshop_killerApp_SE2/src/services/catalogService.ts
+++ Webshop_killerApp_SE2/src/services/catalogService.ts
@@ -2,6 +2,7 @@
 import { autoinject } from 'aurelia-framework';
 import { Category } from '../entities/Category';
 import { Product } from '../entities/Product';
+import { Review } from '../entities/Review';
 
 @autoinject
 export class catalogService {
@@ -35,6 +36,15 @@ export class catalogService {
 
     public async getProduct(Id: number): Promise<Product> {
         let response: Response = await this.http.fetch('catalog/product', {
+            body: json({
+                "Item1": Id
+            })
+        });
+        return await response.json();
+    }
+
+    public async getReviews(Id: number): Promise<Review[]> {
+        let response: Response = await this.http.fetch('catalog/reviews', {
             body: json({
                 "Item1": Id
             })
