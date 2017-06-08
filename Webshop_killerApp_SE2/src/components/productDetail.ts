84bdc0a4dbe6fccdcf6d65e7bd487804c967903f
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-06-08 02:29:46 +0200

    add postedreview field to productDetauls component

--- Webshop_killerApp_SE2/src/components/productDetail.ts
+++ Webshop_killerApp_SE2/src/components/productDetail.ts
@@ -1,7 +1,9 @@
 import { autoinject } from 'aurelia-framework';
+import { AuthService } from 'aurelia-authentication';
 import { catalogService } from '../services/catalogService';
 import { Product } from '../entities/Product';
 import { Review } from '../entities/Review';
+import * as jwt_decode from 'jwt-decode';
 
 @autoinject
 export class ProductDetail {    
@@ -9,11 +11,25 @@ export class ProductDetail {
     private product: Product;
     private reviews: Review[];
 
-    constructor(private catalogService: catalogService) { }
+    private postedReview: Review
+
+    constructor(private catalogService: catalogService, private auth: AuthService)
+    {
+        this.postedReview = new Review();
+    }
 
     async activate(params, routeConfig) {
         console.info(params.id);
         this.product = await this.catalogService.getProduct(params.id);
         this.reviews = await this.catalogService.getReviews(params.id);
+
+        let userid = jwt_decode(this.auth.getAccessToken()).userid;
+
+        for (let item of this.reviews) {
+            if (item.user == userid) {
+                this.postedReview = item;
+                break;
+            }
+        }
     }
 }
