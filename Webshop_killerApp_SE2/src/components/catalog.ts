99089a6b5f5277c883b7e611033f322a22270485
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-06-17 23:50:39 +0200

    add shopping cart to catalog component

--- Webshop_killerApp_SE2/src/components/catalog.ts
+++ Webshop_killerApp_SE2/src/components/catalog.ts
@@ -1,19 +1,20 @@
 import { autoinject } from 'aurelia-framework';
 import { catalogService, PageResponse } from '../services/catalogService';
+import { CartService } from '../services/CartService';
 import { Product } from '../entities/Product';
 import { Category } from '../entities/Category';
 
 @autoinject
 export class Catalog {  
     
-    products: Product[];
-    categories: Category[];
+    products: Product[] = [];
+    categories: Category[] = [];
 
     selectedCategory: number = null;
     pageAmount: number = 1;
     number: number = 1;
 
-    constructor(private catalogService: catalogService) {}
+    constructor(private catalogService: catalogService, private cartService: CartService) { }
 
     async attached() {
         this.categories = await this.catalogService.getTopLevelCategories();
