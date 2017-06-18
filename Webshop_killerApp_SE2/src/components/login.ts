1c50f9762ab5f60791cf19fe9564bd3972aa1c37
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-06-18 18:49:59 +0200

    add register logic in component

--- Webshop_killerApp_SE2/src/components/login.ts
+++ Webshop_killerApp_SE2/src/components/login.ts
@@ -2,20 +2,38 @@ import { RouterConfiguration, Router } from 'aurelia-router';
 import { autoinject } from 'aurelia-framework';
 import { HttpClient, json } from 'aurelia-fetch-client';
 import { FetchConfig, AuthService } from 'aurelia-authentication';
+import { User } from '../entities/User';
+import { UserService } from '../services/UserService';
 
 @autoinject
 export class login {
-    username: string;
-    password: string;
+    username: string = '';
+    password: string = '';
+
+    newUser: User;
+    newPassword: string = '';
 
     message: string;
-    constructor(private http: HttpClient, private auth: AuthService, private router: Router) { }
+    constructor(private http: HttpClient, private auth: AuthService, private userService: UserService, private router: Router) {
+        this.newUser = new User();
+    }
 
     async login() {
 
         await this.auth.login({
             "username": this.username,
             "password": this.password
+        }).catch(Error => {
+            this.message = Error.response;
+        });
+    }
+
+    async register() {
+        await this.userService.Register(this.newUser, this.newPassword);
+
+        await this.auth.login({
+            "username": this.newUser.username,
+            "password": this.newPassword
         }).catch(Error => {
             this.message = Error.response;
         });
