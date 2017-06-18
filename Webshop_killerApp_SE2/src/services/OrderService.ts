076a4a384c85b8614dde1e6b270e3618866d05a1
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-06-19 01:28:12 +0200

    add clientside orderservice

--- Webshop_killerApp_SE2/src/services/OrderService.ts
+++ Webshop_killerApp_SE2/src/services/OrderService.ts
@@ -2,6 +2,8 @@
 import { autoinject } from 'aurelia-framework';
 import { Product } from '../entities/Product';
 
+import { Order } from '../entities/Order';
+
 @autoinject
 export class OrderService {
 
@@ -11,6 +13,26 @@ export class OrderService {
     public async getOrderedProducts(user: number): Promise<Product[]> {
         alert(user);
         let response: Response = await this.http.fetch('/order/products', {
+            body: json({
+                "Item1": user
+            })
+        });
+        return await response.json();
+    }
+
+    public async getProductsByOrder(order: number): Promise<Product[]> {
+        alert(user);
+        let response: Response = await this.http.fetch('/order/products/order', {
+            body: json({
+                "Item1": order
+            })
+        });
+        return await response.json();
+    }
+
+    public async getOrdersByUser(user: number): Promise<Order[]> {
+        alert(user);
+        let response: Response = await this.http.fetch('/order/orders', {
             body: json({
                 "Item1": user
             })
